f72c6d6e1b352a5c966f578da13f7c9c0f82ac2b
Musa Musa <musabrillz@gmail.com>
2019-01-19 09:57:20 +0100

    Implemented a TodoApp using redux

---
+++ lib/redux/actions.dart
@@ -0,0 +1,20 @@
+import 'package:todo_redux/models/model.dart';
+
+class AddItemAction {
+  static int _id = 0;
+  final String item;
+
+  AddItemAction(this.item) {
+    _id++;
+  }
+
+  int get id => _id;
+}
+
+class RemoveItemAction {
+  final Item item;
+
+  RemoveItemAction(this.item);
+}
+
+class RemoveItemsAction {}
